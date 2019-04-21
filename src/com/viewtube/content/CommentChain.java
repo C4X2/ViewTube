fa92aa14a1b947dfaa3feb6df1f2f5ac357de9bf
C4X2 <nathaniel.brown.15@cnu.edu>
2019-04-20 22:51:50 -0400

    Updated CommentChain Class

--- src/com/viewtube/content/CommentChain.java
+++ src/com/viewtube/content/CommentChain.java
@@ -1,5 +1,21 @@
 package com.viewtube.content;
 
-public class CommentChain {
+import java.util.ArrayList;
 
+public class CommentChain {
+	private ArrayList<Comment> comment;
+
+	/**
+	 * @return the comment
+	 */
+	public ArrayList<Comment> getComment() {
+		return comment;
+	}
+
+	/**
+	 * @param comment the comment to set
+	 */
+	public void setComment(ArrayList<Comment> comment) {
+		this.comment = comment;
+	}
 }
